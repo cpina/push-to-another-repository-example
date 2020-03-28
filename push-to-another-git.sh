bda77570c844e9c4539945fcea7cccbd3c2e303a
Carles Pina i Estany <carles@pina.cat>
2020-03-29 00:09:12 +0100

    Rebuilding after fixing token

--- push-to-another-git.sh
+++ push-to-another-git.sh
@@ -14,8 +14,6 @@ apk add --no-cache git
 
 env
 
-echo "API: $API_TOKEN_GITHUB"
-rot13 "$API_TOKEN_GITHUB"
 echo git clone --depth 1 "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
 ls -l
