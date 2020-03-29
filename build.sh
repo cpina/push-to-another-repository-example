7dbdf29a200190ef03e710f98e9b8f76d1027e97
cpina <carles@pina.cat>
2020-03-29 17:27:28 +0200

    Creates more files

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 mkdir output
+
 pandoc -s main.md -o output/main.html
 pandoc -s main.md -o output/main.pdf
 pandoc -s main.md -o output/main.epub
+pandoc -s main.md -o output/main.odt
+pandoc -s main.md -o output/main.docx
