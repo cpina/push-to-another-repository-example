92383d861261c0212bcaebce347abeb3ef6a9db0
Carles Pina i Estany <carles@pina.cat>
2021-12-08 23:47:03 +0100

    Add test file

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ pandoc -s main.md -o output/main.docx
 
 date > output/.build_date.txt
 
+echo "A test file" > output/test-file.txt
+
 echo "generated_at: $(date)" > variables.yml
 
 mustache variables.yml index.output.html > output/index.html
