319f164658d37065df300fe817f60a43e330c2b4
Carles Pina i Estany <carles@pina.cat>
2022-09-14 19:37:30 +0100

    Try to reproduce problem found in https://github.com/cpina/github-action-push-to-another-repository/issues/78

--- build.sh
+++ build.sh
@@ -16,6 +16,3 @@ echo "generated_at: $(date)" > variables.yml
 mustache variables.yml index.output.html > output/index.html
 mustache variables.yml README.output.md > output/README.md
 
-mkdir a_test
-cd a_test
-
