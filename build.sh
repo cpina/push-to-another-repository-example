3575bc0348f4c4679e83a19304be89498120eaa1
Carles Pina i Estany <carles@pina.cat>
2022-09-14 19:22:36 +0100

    Debugging if changing the directory is changing the pwd
    Trying to reproduce https://github.com/cpina/github-action-push-to-another-repository/issues/78#issuecomment-1245387171

--- build.sh
+++ build.sh
@@ -16,3 +16,6 @@ echo "generated_at: $(date)" > variables.yml
 mustache variables.yml index.output.html > output/index.html
 mustache variables.yml README.output.md > output/README.md
 
+mkdir a_test
+cd a_test
+
